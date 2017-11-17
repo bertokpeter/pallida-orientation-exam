01ad300e8afa47e49cb9a0dfc119f94e4451589e
bertokpeter <bertokpeter92@gmail.com>
2017-11-17 12:48:38 +0100

    feat(licence server): validator function

--- licence_server.js
+++ licence_server.js
@@ -28,30 +28,39 @@ app.get('/', function(req, res){
 });
 
 app.get('/search', function(req, res){
-    let selector = 'SELECT * FROM licence_plates WHERE plate LIKE "%' + req.query.q + '%"';
-    if (req.query.police){
-        selector += 'AND plate LIKE "RB%"'
-    } else if (req.query.diplomat){
-        selector += 'AND plate LIKE "DT%"'
-    }
-    conn.query(selector, function(err, rows){
-        if(err) {
-            console.log(err.toString());
+    let isItValid = validator(req.query.q);
+    console.log(isItValid);
+    if (isItValid){
+        let selector = 'SELECT * FROM licence_plates WHERE plate LIKE "%' + req.query.q + '%"';
+        if (req.query.police){
+            selector += 'AND plate LIKE "RB%"'
+        } else if (req.query.diplomat){
+            selector += 'AND plate LIKE "DT%"'
         }
-        console.log("Data received from Db:\n");
-        let response = {"result": 'ok',
-                        "data":[]};
-        rows.forEach(function(row){
-            response.data.push({
-                "licence": row.plate,
-                "brand": row.car_brand,
-                "model": row.car_model,
-                "year": row.year,
-                "color": row.color
+        conn.query(selector, function(err, rows){
+            if(err) {
+                console.log(err.toString());
+            }
+            console.log("Data received from Db:\n");
+            let response = {"result": 'ok',
+                            "data":[]};
+            rows.forEach(function(row){
+                response.data.push({
+                    "licence": row.plate,
+                    "brand": row.car_brand,
+                    "model": row.car_model,
+                    "year": row.year,
+                    "color": row.color
+                });
             });
+            res.json(response);
         });
+    } else {
+        let response = {
+            "result": "error", "message": "invalid input"
+        }
         res.json(response);
-    });
+    }
 });
 
 app.get('/search/:brand', function(req, res){
@@ -75,5 +84,23 @@ app.get('/search/:brand', function(req, res){
         res.json(response);
     });
 });
+
+function validator(licence_plate){
+    console.log(licence_plate);
+    let allowedChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789';
+    let upperCasePlate = licence_plate.toUpperCase()
+    console.log(upperCasePlate);
+    if (upperCasePlate.length > 7){
+        return false;
+    } else {
+        for (let i = 0; i < upperCasePlate.length; i++){
+            if (!allowedChars.includes(upperCasePlate[i])){
+                console.log('hali');
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 app.listen(3000);
